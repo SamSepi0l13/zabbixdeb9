1a815f2aa2a61e8054f1c9ecfd585f26113259ef
SamSepi0l13 <134660568+SamSepi0l13@users.noreply.github.com>
2023-10-22 22:31:46 -0300

    Create zabbixdeb9.sh

---
+++ zabbixdeb9.sh
@@ -0,0 +1,20 @@
+# Lógica de instalação do Agente Zabbix no Debian 9
+echo "Instalando o Agente Zabbix no Debian 9..."
+
+# Adicionar o repositório do Zabbix e atualizar o sistema no Debian 9
+echo "Adicionando o repositório do Zabbix e atualizando o sistema no Debian 9..."
+wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-4+debian9_all.deb
+dpkg -i zabbix-release_6.0-4+debian9_all.deb
+apt update
+
+# Instalar o Agente Zabbix 2 e seus plugins
+echo "Instalando o Agente Zabbix 2 e seus plugins no Debian 9..."
+apt install zabbix-agent2 zabbix-agent2-plugin-*
+
+# Iniciar o serviço do Agente Zabbix 2
+echo "Iniciando o serviço do Agente Zabbix 2..."
+systemctl start zabbix-agent2
+
+# Habilitar o serviço para iniciar automaticamente na inicialização do sistema
+echo "Habilitando o serviço para iniciar automaticamente na inicialização do sistema..."
+systemctl enable zabbix-agent2
